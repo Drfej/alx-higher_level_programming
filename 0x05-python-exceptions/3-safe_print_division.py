eaf8c087e86afc5dedac95abf4a06b4c643caf0e
FUNSHO <fejjy1998@gmail.com>
2022-06-20 19:14:52 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = 0
     try:
-        res = a / b
-    except ZeroDivisionError:
-        res = None
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
     finally:
-        print("Inside result: {}".format(res))
-        return 
+        print("Inside result: {}".format(div))
+    return div
